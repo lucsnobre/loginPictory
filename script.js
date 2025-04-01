417c78b7e518d7cf6c0957fe1d31c725be6a838e
LUCAS RODRIGUES NOBRE SILVA <24122499@CFP127ALUNO.LOCAL>
2025-04-01 08:49:30 -0300

    js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-document.getElementById('signup-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Cadastro realizado com sucesso!');
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("signup-form")
+    const emailInput = document.getElementById("email")
+    const passwordInput = document.getElementById("password")
+    
+    function submitForm(event) {
+        event.preventDefault()
 
+        const email = emailInput.value;
+        const password = passwordInput.value;
+        
+        const formData = {
+            email: email,
+            password: password
+        }
+        
+        // Exibir os dados no console
+        console.log("Dados do cadastro:", formData)
+        
+        // Limpar os campos do formulário
+        emailInput.value = "";
+        passwordInput.value = "";
+    }
+    
+    form.addEventListener("submit", submitForm);
+    
+    form.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            submitForm(event);
+        }
+    });
+});
