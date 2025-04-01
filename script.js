8aebb01811a2faa13d6ac6545d561e59d07d1cec
LUCAS RODRIGUES NOBRE SILVA <24122499@CFP127ALUNO.LOCAL>
2025-04-01 09:38:27 -0300

    ela nao é sua nem e minha

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("signup-form");
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
+    const form = document.getElementById("signup-form")
+    const emailInput = document.getElementById("email")
+    const passwordInput = document.getElementById("password")
 
     async function submitForm(event) {
-        event.preventDefault();
+        event.preventDefault()
 
-        const email = emailInput.value;
-        const password = passwordInput.value;
+        const email = emailInput.value
+        const password = passwordInput.value
+
+        // {
+        //     "nome":"Vitor Amato",
+        //     "email": "vitor@jesus.com",
+        //     "senha":"Amato",
+        //     "premium":"1",
+        //     "imagemPerfil":"https://assets.propmark.com.br/uploads/2022/02/WhatsApp-Image-2022-02-18-at-08.52.06.jpeg",
+        //     "senhaRecuperacao": "Gato12"
+        // }
 
         const formData = {
+            nome: "test tesd",
             email: email,
-            password: password
-        };
+            senha: password,
+            premium:"1",
+            imagemPerfil:"https://assets.propmark.com.br/uploads/2022/02/WhatsApp-Image-2022-02-18-at-08.52.06.jpeg",
+            senhaRecuperacao: "Gato12"
+        }
+
+        const url = "https://back-spider.vercel.app/user/cadastrarUser"
+
+        const url2 = "http://localhost:8080/user/cadastrarUser"
 
         try {
-            const response = await fetch('http://localhost:8080/user/cadastrarUser', {
+            const response = await fetch(url, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -23,25 +40,24 @@ document.addEventListener("DOMContentLoaded", function () {
                 body: JSON.stringify(formData),
             })
 
+            console.log(response);
+            
+
             if (response.ok) {
-                const data = await response.json();
-                console.log('Resposta da API:', data);
-                // Aqui você pode adicionar código para lidar com a resposta da API, como exibir uma mensagem de sucesso ou redirecionar o usuário.
+                const data = await response.json()
+                console.log('Resposta da API:', data)
             } else {
-                console.error('Erro ao enviar dados:', response.statusText);
-                // Aqui você pode adicionar código para lidar com erros, como exibir uma mensagem de erro para o usuário.
+                console.error('Erro ao enviar dados:', response.statusText)
             }
         } catch (error) {
-            console.error('Erro ao enviar dados:', error);
-            // Aqui você pode adicionar código para lidar com erros de rede ou outros problemas.
+            console.error('Erro ao enviar dados:', error)
         } finally {
-            // Limpar os campos do formulário após o envio
-            emailInput.value = "";
-            passwordInput.value = "";
+            emailInput.value = ""
+            passwordInput.value = ""
         }
     }
 
-    form.addEventListener("submit", submitForm);
+    form.addEventListener("submit", submitForm)
 
     form.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
